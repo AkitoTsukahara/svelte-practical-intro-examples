780d68033f191012d44b58c21a27778f5e614bf3
a-tsukahara <a-tsukahara@MAC28.local>
2023-06-04 23:57:38 +0900

    chapter1

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,125 @@
-<script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script lang="ts">
+  import Slider from './Slider.svelte';
+
+  let cart = [];
+  let relatedProducts = [
+    { id: 'react-book', name: 'React Book', price: 3500 },
+    { id: 'vue-book', name: 'Vue Book', price: 3500 },
+    { id: 'angular-book', name: 'Angular Book', price: 3500 },
+  ];
+  let product = {
+    id: 'svelte-book',
+    name: 'Svelte Guide',
+    price: 3500,
+    images: [
+        '/src/assets/svelte-book-1.png',
+        '/src/assets/svelte-book-2.png',
+        '/src/assets/svelte-book-3.png',
+    ]
+  }
+
+  function addToCart(productId: number) {
+    cart = [...cart, productId];
+  }
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<header class="header">
+  <a class="header-title" href="/">Svelte EC</a>
+  <nav>
+    <ul class="header-links">
+      <li>ようこそゲストさん</li>
+      <li><a href="/cart">カート (0)</a></li>
+    </ul>
+  </nav>
+</header>
+
+<article class="product">
+  <div class="product-main">
+    <div class="image-container">
+      <Slider images={product.images} />
+    </div>
+
+    <div>
+      <h2>{product.name}</h2>
+      <dl>
+        <dt>価格</dt>
+        <dd>{product.price}円</dd>
+      </dl>
+      <div>
+        {#if !cart.includes(product.id)}
+          <button on:click={() => addToCart('svelte-book')}>カートに入れる</button>
+        {:else}
+          <button on:click={() => cart = cart.filter(item => item !== 'svelte-book')}>カートから削除</button>
+        {/if}
+      </div>
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+  <footer>
+    <h3>関連商品</h3>
+    <ul>
+      {#each relatedProducts as product}
+        <li>
+          <a href={`/products/${product.id}`}>{product.name}</a>
+          - {product.price}円
+        </li>
+      {/each}
+    </ul>
+  </footer>
+</article>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  :global(body) {
+    margin: 0;
+    background-color: #eee;
+    padding: 0;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+    background-color: #fff;
+    padding: 0 15px;
+    width: 100%;
+    max-width: 800px;
+    height: 50px;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+
+  .header-title {
+    font-weight: bold;
   }
-  .read-the-docs {
-    color: #888;
+
+  .header-links {
+    display: flex;
+    gap: 10px;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  .product {
+    margin: 0 auto;
+    background-color: #fff;
+    padding: 15px;
+    width: 100%;
+    max-width: 800px;
+  }
+
+  .product-main {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+  }
+
+  .image-container {
+    width: 100%;
+    max-width: 400px;
+    overflow: hidden;
+  }
+
+  .image-container img {
+    width: 100%;
   }
 </style>
